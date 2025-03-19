5b41a3738ce3316d72757c53ac85e5c13a676bed
Mattheus Adhonnay <mamenezes@viceri.com.br>
2025-03-19 12:53:35 -0300

    feat: api integration

--- web/src/app/invite/[subcribeId]/page.tsx
+++ web/src/app/invite/[subcribeId]/page.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
-import logo from "../../assets/logo.svg";
+import logo from "../../../assets/logo.svg";
 import { InviteLinkInput } from "./invite-link-input";
-import Ranking from "./ranking";
+import { Ranking } from "./ranking";
 import { Stats } from "./stats";
 
-export default function InvitePage() {
-  const inviteLink = "http://localhost:3000/invite/3291381203801293812313";
+interface InvitePageProps {
+  params: Promise<{
+    subcribeId: string;
+  }>;
+}
+
+export default async function InvitePage(props: InvitePageProps) {
+  const { subcribeId } = await props.params;
+
+  const inviteLink = `http://localhost:3333/invites/${subcribeId}`;
 
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
@@ -36,7 +44,7 @@ export default function InvitePage() {
 
         <InviteLinkInput inviteLink={inviteLink} />
 
-        <Stats />
+        <Stats subcribeId={subcribeId} />
       </div>
 
       <Ranking />
